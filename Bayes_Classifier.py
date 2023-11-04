9fea7e0dc388d6a3c664a8fda1b8c1b215779912
Farah Mohamed <101725658+farah50@users.noreply.github.com>
2023-11-04 10:34:20 +0200

    Update Bayes_Classifier.py

--- Bayes_Classifier.py
+++ Bayes_Classifier.py
@@ -3,13 +3,19 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 
 iris = load_iris()
-X = iris.data
-y = iris.target
+X = iris.data          # X represents Data
+Y = iris.target        # Y represents labels
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=40)
+
+# def train_test_split(data, labels, testRatio = 0.3, valRatio = 0.3):
 
 
 classifier = GaussianNB()
-classifier.fit(X_train, y_train)
+classifier.fit(X_train, Y_train)
+
+# def calculate_accuracy(predicted_y, y):
+
+# Draw decision boundaries
 
 
